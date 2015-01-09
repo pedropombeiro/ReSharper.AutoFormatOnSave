c72174241f0d05401b4218420aa293b41a73754d
Bart Joy <bart@ecargo.co.nz>
2015-01-09 15:06:56 +1300

    Only apply formatting when Visual Studio is the foreground window to avoid taking focus off another window that the user may have switched to.

---
+++ NativeMethods.cs
@@ -0,0 +1,14 @@
+﻿namespace ReSharper.AutoFormatOnSave
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    internal class NativeMethods
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+    }
+}
