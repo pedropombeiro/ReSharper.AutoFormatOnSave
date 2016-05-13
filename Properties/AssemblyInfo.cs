2e50bdd5a2a19c96ab6ad853e4a996e245fa6225
Pedro Pombeiro <pedropombeiro@gmail.com>
2016-05-13 14:43:00 +0200

    Update version to 1.2.4.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Developer In The Flow")]
 [assembly: AssemblyProduct("ReSharper.AutoFormatOnSave")]
-[assembly: AssemblyCopyright("Copyright © Pedro Pombeiro 2012-2015")]
+[assembly: AssemblyCopyright("Copyright © Pedro Pombeiro 2012-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.2.3.0")]
-[assembly: AssemblyFileVersion("1.2.3.0")]
+[assembly: AssemblyVersion("1.2.4.0")]
+[assembly: AssemblyFileVersion("1.2.4.0")]
